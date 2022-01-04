722c83b3684d77a544ab32c7f31a9d9d4eee39f0
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2022-01-04 13:06:30 -0300

    Update BotController.php

--- application/controllers/app/BotController.php
+++ application/controllers/app/BotController.php
@@ -18,7 +18,7 @@ class BotController extends Controller
         $this->addTitle('Bot');
 
         $opr = new Operacion();
-        $ds = $opr->getDataset('idusuario = '.$auth->get('idusuario'),'symbol');
+        $ds = $opr->getDataset('idusuario = '.$auth->get('idusuario'),'auto_restart DESC, symbol');
         
         $dg = new HtmlTableDg(null,null,'table table-hover table-striped');
         $dg->addHeader('Moneda');
