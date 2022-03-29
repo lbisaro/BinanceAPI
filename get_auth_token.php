de2b27478e23727aac7ef91397a19566036408cb
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2022-03-29 11:27:17 -0300

    Create get_auth_token.php

---
+++ get_auth_token.php
@@ -0,0 +1,4 @@
+<?php
+$redirectUri = 'http://bisaro.ar/get_oauth_token.php';
+$clientId = '840728873329-k3edk7jc7u7itl3cas9dmgtmc0crv0k8.apps.googleusercontent.com';
+$clientSecret = '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';
