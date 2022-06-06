bdc47c9bacc2784b4772cf9fdf3228400d656e30
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2022-06-06 17:37:58 -0300

    Update functions.php

--- library/functions.php
+++ library/functions.php
@@ -1317,4 +1317,30 @@ function diferenciaFechasEnHoras($fecha_inicial,$fecha_final)
     return $horas;
 }
 
-?>
+function getIPClient()
+{
+    if (isset($_SERVER["HTTP_CLIENT_IP"]))
+    {
+        return $_SERVER["HTTP_CLIENT_IP"];
+    }
+    elseif (isset($_SERVER["HTTP_X_FORWARDED_FOR"]))
+    {
+        return $_SERVER["HTTP_X_FORWARDED_FOR"];
+    }
+    elseif (isset($_SERVER["HTTP_X_FORWARDED"]))
+    {
+        return $_SERVER["HTTP_X_FORWARDED"];
+    }
+    elseif (isset($_SERVER["HTTP_FORWARDED_FOR"]))
+    {
+        return $_SERVER["HTTP_FORWARDED_FOR"];
+    }
+    elseif (isset($_SERVER["HTTP_FORWARDED"]))
+    {
+        return $_SERVER["HTTP_FORWARDED"];
+    }
+    else
+    {
+        return $_SERVER["REMOTE_ADDR"];
+    }
+}
