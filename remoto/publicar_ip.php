003a23d83c880a47a8f3c0c044bad3c151cfd4bf
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2022-07-10 23:21:25 -0300

    Update publicar_ip.php

--- remoto/publicar_ip.php
+++ remoto/publicar_ip.php
@@ -1,4 +1,28 @@
 <?php
+chdir('../');
+include_once("config.php");
+
+include_once(LIB_PATH."functions.php");
+include_once(LIB_PATH."Sql.php");
+include_once(MDL_PATH."usr/UsrUsuario.php");
+
 $ip = file_get_contents('https://wgetip.com');
-echo "\n".file_get_contents('http://bisaro.ar/receive_ip.php?ip='.$ip);
-echo "\nIP registrada: ".$ip;
+$rsp = @file_get_contents('http://bisaro.ar/receive_ip.php?ip='.$ip);
+if ($rsp)
+{
+    echo "\n".$rsp;
+    echo "\nIP registrada: ".$ip;
+}
+else
+{
+    Sql::Connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+    include_once (MDL_PATH."NotificacionApp.php");
+    $usr = new UsrUsuario(NotificacionApp::SUPER_ADMIN_ID);
+    $registration_ids[] = $usr->getFCM_token();
+    
+    $title = 'ALERTA!';
+    $body = 'bisaro.ar - Fuera de servicio'."\n".$lockFileText;
+    $result = NotificacionApp::send($title,$body,$registration_ids);
+    
+}
