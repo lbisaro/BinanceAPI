d2b67242f9d9bbe578a8871a753e32edfb1726a3
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2022-07-20 19:27:08 -0300

    Update 2022.07.20 Operacion Capital In USD.sql

--- mysql/2022.07.20 Operacion Capital In USD.sql
+++ mysql/2022.07.20 Operacion Capital In USD.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `operacion` 
     ADD `base_start_in_usd` DECIMAL(15,8) NULL DEFAULT NULL AFTER `stop`,
-    ADD `quote_start_in_usd` DECIMAL(15,8) NOT NULL DEFAULT ,
-    ADD `start` DATETIME NULL DEFAULT NULL AFTER `capital_in_usd`;
+    ADD `quote_start_in_usd` DECIMAL(15,8) NULL DEFAULT NULL,
+    ADD `start` DATETIME NULL DEFAULT NULL ;
