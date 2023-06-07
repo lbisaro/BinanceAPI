78eabf2e45be95ce97df99683cd6dd9ba77e6e95
Leonardo Bisaro <leonardo.bisaro@gmail.com>
2023-06-07 17:28:36 -0300

    Update crontab_notificacionApp.php

--- crontab/crontab_notificacionApp.php
+++ crontab/crontab_notificacionApp.php
@@ -3,7 +3,6 @@ include_once (MDL_PATH."NotificacionApp.php");
 
 $usr = new UsrUsuario(NotificacionApp::SUPER_ADMIN_ID);
 $registration_ids[] = $usr->getFCM_token();
-//$forcedToken = 'eMNllRsKTyKLSahgo4CgKI:APA91bG85TMw2rc_Kzak7LXjTqgjzj6EgGs7TPzh6fp9xgwlMDFvvtBdDDbLzb_ixC8RPW4gFkp29mYQyf6-eVHRyGXbS2QWDIM_ZGUp7sOwOijj8TOfx0Bzt1BFF16JXwdTnGvkAdaN';
 $forcedToken = 'fjM2sgYUTxKd-T7CIQtN7T:APA91bGwjNOZYGJKsjSv5VlMzuAghJ1zq_06vin7QI-fZWUgR-f9DW0HPUA3ZKrQJ-MxS3vwcm-q1CmpssvvTiv0VUfKUvl32o_DcIwZncnDH1bI6k8HwN-B99DYQWqiuV-QvBhY3auh';
 if (!in_array($forcedToken, $registration_ids))
     $registration_ids[] = $forcedToken;
